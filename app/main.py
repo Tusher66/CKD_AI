918a50e6c92f9bf2460c3156fbcbeb1578ab2ce9
Ismail Hossain Tusher <80039087+Tusher66@users.noreply.github.com>
2026-08-06 16:42:10 +0600

    Fast api create.

---
+++ app/main.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI
+import joblib
+from pydantic import BaseModel
+import pandas as pd
+from app.services.model_loader import model, scaler
+
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+
+    return {
+        "message":"Hello AI"
+    }
+
+
+# model = joblib.load("model/ckd_model.joblib")
+
+# scaler = joblib.load("model/scaler.joblib")
+
+class Patient(
+    BaseModel
+):
+    Age:int
+    BP:int
+    Creatinine:float
+
+@app.post("/predict")
+def predict(
+predict:Patient
+):
+
+    data = pd.DataFrame({
+        "Age":[predict.Age],
+        "BP":[predict.BP],
+        "Creatinine":[predict.Creatinine]
+    })
+
+    # data = scaler.transform(
+    #     data
+    # )
+
+    # prediction = model.predict(
+    #     data
+    # )
+
+    data = scaler.transform(data)
+
+    prediction = model.predict(data)
+
+    return{
+        "Prediction":int(
+            prediction[0]
+        )
+    }
+    
+    # return {
+    #     "Age":predict.Age,
+    #     "BP":predict.BP,
+    #     "Creatinine":predict.Creatinine
+    # }
---
+++ app/services/model_loader.py
@@ -0,0 +1,4 @@
+import joblib
+
+model = joblib.load("model/ckd_model.joblib")
+scaler = joblib.load("model/scaler.joblib")
